66310b0f124d8d78f5ef690863028e8d7a1e141e
Ivan Titov <risenforce@mail.ru>
2020-04-13 01:02:38 +0500

    Increment HDA Version
    Save changes to new HDA #28
    Fixed #40 #39

--- python2.7libs/houdini_tdk/new_hda_version.py
+++ python2.7libs/houdini_tdk/new_hda_version.py
@@ -34,6 +34,54 @@ except ImportError:
 import hou
 
 
+def versionByTypeName(name):
+    split_count = name.count('::')
+    if split_count == 2:
+        version = name.split('::')[-1]
+    elif split_count == 1 and name[-1].isdigit():
+        version = name.split('::')[-1]
+    else:
+        version = '1.0'
+    return version
+
+
+def nextVersion(version, component=0):
+    if isinstance(version, basestring):
+        values = [int(value) for value in version.split('.')]
+    elif isinstance(version, (list, tuple)):
+        values = version
+    else:
+        raise TypeError
+    values[component] += 1
+    values = values[:component + 1] + [0] * (len(values) - component - 1)
+    return '.'.join(str(value) for value in values)
+
+
+def incrementHDAVersion(node, component):
+    node_type = node.type()
+    name = node_type.name()
+    version = nextVersion(versionByTypeName(name), component)
+    split_count = name.count('::')
+    definition = node_type.definition()
+    if split_count == 2:
+        new_type_name = '::'.join(name.split('::')[:2])
+    elif split_count == 1 and name[-1].isdigit():
+        new_type_name = name.split('::')[0]
+    else:
+        new_type_name = name
+    new_type_name += '::' + version
+    file = definition.libraryFilePath()
+    ext = os.path.splitext(file)[-1]
+    new_file_name = new_type_name.replace(':', '_').replace('.', '_') + ext
+    new_file = os.path.join(os.path.dirname(file), new_file_name).replace('\\', '/')
+    print(new_file)
+    definition.copyToHDAFile(new_file, new_type_name)
+    hou.hda.installFile(new_file)
+    new_definition = hou.hda.definitionsInFile(new_file)[0]
+    new_definition.updateFromNode(node)
+    node.changeNodeType(new_type_name, keep_network_contents=False)
+
+
 class NewVersionDialog(QWidget):
     def __init__(self, node, parent=None):
         super(NewVersionDialog, self).__init__(parent, Qt.Window)
@@ -42,14 +90,7 @@ class NewVersionDialog(QWidget):
 
         # Data
         self.node = node
-        hda_name = node.type().name()
-        self.split_count = hda_name.count('::')
-        if self.split_count == 2:
-            self.curr_version = hda_name.split('::')[-1]
-        elif self.split_count == 1 and hda_name[-1].isdigit():
-            self.curr_version = hda_name.split('::')[-1]
-        else:
-            self.curr_version = '1.0'
+        self.curr_version = versionByTypeName(node.type().name())
         self.next_version = None
 
         # Layout
@@ -83,35 +124,21 @@ class NewVersionDialog(QWidget):
         layout.addWidget(increment_button)
 
     def updateNextVersion(self):
-        values = [int(value) for value in self.curr_version.split('.')]
-        values[self.comp_slider.value()] += 1
-        self.next_version = '.'.join(str(value) for value in values)
+        self.next_version = nextVersion(self.curr_version, self.comp_slider.value())
         self.next_version_label.setText('Next Version ' + self.next_version)
 
     def _increment(self):
-        node_type = self.node.type()
-        name = node_type.name()
-        definition = node_type.definition()
-        if self.split_count == 2:
-            new_name = '::'.join(name.split('::')[:2]) + '::' + self.next_version
-        elif self.split_count == 1 and name[-1].isdigit():
-            new_name = '::'.join(name.split('::')[0]) + '::' + self.next_version
-        else:
-            new_name = name + '::' + self.next_version
-        file = definition.libraryFilePath()
-        ext = os.path.splitext(file)[-1]
-        new_file_name = new_name.replace(':', '_').replace('.', '_') + ext
-        new_file = os.path.join(os.path.dirname(file), new_file_name).replace('\\', '/')
-        definition.copyToHDAFile(new_file, new_name)
-        hou.hda.installFile(new_file)
-        self.node.changeNodeType(new_name, keep_network_contents=False)
+        incrementHDAVersion(self.node, self.comp_slider.value())
         hou.ui.setStatusMessage('HDA version successfully incremented',
                                 hou.severityType.ImportantMessage)
         self.close()
 
 
-def incrementHdaVersion(**kwargs):
-    nodes = hou.selectedNodes()
+def showNewVersionDialog(**kwargs):
+    if 'node' in kwargs:
+        nodes = kwargs['node'],
+    else:
+        nodes = hou.selectedNodes()
     if not nodes:
         raise hou.Error('No node selected')
     elif len(nodes) > 1:
