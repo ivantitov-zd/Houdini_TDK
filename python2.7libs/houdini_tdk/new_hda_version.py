9773c0f023aa53b54e422492cdb905bf1f6f574f
Ivan Titov <risenforce@mail.ru>
2020-04-14 19:07:43 +0500

    Increment HDA Version
    Better UI #20
    Additional information #19
    Allow expand version #43

--- python2.7libs/houdini_tdk/new_hda_version.py
+++ python2.7libs/houdini_tdk/new_hda_version.py
@@ -52,39 +52,49 @@ def nextVersion(version, component=0):
         values = version
     else:
         raise TypeError
-    values[component] += 1
+    try:
+        values[component] += 1
+    except IndexError:
+        values += [0] * (component - len(values)) + [1]
     values = values[:component + 1] + [0] * (len(values) - component - 1)
     return '.'.join(str(value) for value in values)
 
 
-def incrementHDAVersion(node, component):
-    node_type = node.type()
-    name = node_type.name()
-    version = nextVersion(versionByTypeName(name), component)
-
+def nextVersionTypeName(name, component):
+    next_version = nextVersion(versionByTypeName(name), component)
     split_count = name.count('::')
-    definition = node_type.definition()
     if split_count == 2:
         new_type_name = '::'.join(name.split('::')[:2])
     elif split_count == 1 and name[-1].isdigit():
         new_type_name = name.split('::')[0]
     else:
         new_type_name = name
+    new_type_name += '::' + next_version
+    return new_type_name
 
-    new_type_name += '::' + version
 
+def incrementHDAVersion(node, component):
+    node_type = node.type()
+    name = node_type.name()
+
+    new_type_name = nextVersionTypeName(name, component)
+
+    definition = node_type.definition()
     file = definition.libraryFilePath()
     ext = os.path.splitext(file)[-1]
     new_file_name = new_type_name.replace(':', '_').replace('.', '_') + ext
     new_file = os.path.join(os.path.dirname(file), new_file_name).replace('\\', '/')
     definition.copyToHDAFile(new_file, new_type_name)
+
     hou.hda.installFile(new_file)
     new_definition = hou.hda.definitionsInFile(new_file)[0]
+
     new_definition.updateFromNode(node)
+
     node.changeNodeType(new_type_name, keep_network_contents=False)
 
 
-class NewVersionDialog(QWidget):
+class NewVersionDialog(QDialog):
     def __init__(self, node, parent=None):
         super(NewVersionDialog, self).__init__(parent, Qt.Window)
 
@@ -93,42 +103,54 @@ class NewVersionDialog(QWidget):
 
         # Data
         self.node = node
-        self.curr_version = versionByTypeName(node.type().name())
-        self.next_version = None
+        self.src_version = versionByTypeName(node.type().name())
 
         # Layout
-        layout = QVBoxLayout(self)
-        layout.setContentsMargins(4, 4, 4, 4)
-        layout.setSpacing(4)
+        main_layout = QVBoxLayout(self)
+        main_layout.setContentsMargins(4, 4, 4, 4)
+        main_layout.setSpacing(4)
 
-        # Current
-        curr_node_label = QLabel('Current Node ' + node.path())
-        layout.addWidget(curr_node_label)
+        form_layout = QFormLayout()
+        form_layout.setContentsMargins(0, 0, 0, 0)
+        form_layout.setSpacing(4)
+        form_layout.setHorizontalSpacing(8)
+        main_layout.addLayout(form_layout)
 
-        curr_version_label = QLabel('Current Version ' + self.curr_version)
-        layout.addWidget(curr_version_label)
+        # Source
+        src_node_label = QLabel(node.path())
+        form_layout.addRow('Source Node', src_node_label)
+
+        src_name_label = QLabel(self.node.type().name())
+        form_layout.addRow('Source Name', src_name_label)
 
         # Component
         self.comp_slider = QSlider(Qt.Horizontal)
-        self.comp_slider.setMaximum(self.curr_version.count('.'))
-        self.comp_slider.valueChanged.connect(self.updateNextVersion)
-        layout.addWidget(self.comp_slider)
+        self.comp_slider.setMaximum(max(3, self.src_version.count('.')))
+        self.comp_slider.valueChanged.connect(self._updateDestFields)
+        form_layout.addRow('Component', self.comp_slider)
 
-        # Next
-        self.next_version_label = QLabel()
-        self.updateNextVersion()
-        layout.addWidget(self.next_version_label)
+        # Destination
+        self.dst_name_label = QLabel()
+        form_layout.addRow('Dest Name', self.dst_name_label)
+
+        self.dst_file_path = QLabel()
+        form_layout.addRow('Dest File Path', self.dst_file_path)
+
+        self._updateDestFields()
 
         spacer = QSpacerItem(0, 0, QSizePolicy.Ignored, QSizePolicy.Expanding)
-        layout.addSpacerItem(spacer)
+        main_layout.addSpacerItem(spacer)
 
         increment_button = QPushButton('Increment')
         increment_button.clicked.connect(self._increment)
-        layout.addWidget(increment_button)
+        main_layout.addWidget(increment_button)
 
-    def updateNextVersion(self):
-        self.next_version = nextVersion(self.curr_version, self.comp_slider.value())
-        self.next_version_label.setText('Next Version ' + self.next_version)
+    def _updateDestFields(self):
+        new_type_name = nextVersionTypeName(self.node.type().name(), self.comp_slider.value())
+        self.dst_name_label.setText(new_type_name)
+        src_location = os.path.dirname(self.node.type().definition().libraryFilePath())
+        dst_file_path = os.path.join(src_location, new_type_name.replace(':', '_').replace('.', '_')).replace('\\', '/') + '.hda'
+        self.dst_file_path.setText(dst_file_path)
 
     def _increment(self):
         incrementHDAVersion(self.node, self.comp_slider.value())
